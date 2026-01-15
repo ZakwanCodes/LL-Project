429ba2e76a4efeb87ff442ee52afe9c1f74d90ff
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2026-01-15 17:23:37 -0500

    .env practice

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
-import express from "express"
+import express from "express";
+import "dotenv/config";
+
 
 
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) =>{
     res.send("hi");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
