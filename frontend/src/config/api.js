ba12223bfb01eab2d605ffb76efd707fe6f8add0
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2026-02-17 16:23:51 -0500

    placed url for deployment

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,8 @@
-const API_BASE_URL = "https://ll-project-dna5.onrender.com";
+const API_BASE_URL = "https://ll-backend-t0jy.onrender.com";
 
 export const API_ROUTES = {
   auth: `${API_BASE_URL}/api/auth`,
   loomian: `${API_BASE_URL}/api/loomian`,
   inventory: `${API_BASE_URL}/api/inventory`,
 };
+ 
