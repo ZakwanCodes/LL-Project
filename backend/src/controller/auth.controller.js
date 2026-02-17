27b2ba8416e9395388ad7dd4db556a52ea419637
Zakwan Dadabhai <zakwandadabhai@gmail.com>
2026-02-17 17:40:05 -0500

    test

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -73,7 +73,7 @@ export async function login(req, res) {
         res.cookie("jwt", token, {
             maxAge : 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             secure: process.env.NODE_ENV === "production",
         });
         
